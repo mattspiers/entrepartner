03d5a8c56f1d3d40f8ca909466702abf70657c26
Matthew Spiers <mspiers@ru1e.com>
2013-11-17 21:49:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Entrepartner::Application.config.secret_key_base = '1f767ea9b16c17a4be3789f2b92bf2de51519bd8699ed14f15cc355e999c5bce831bc7d8666514fa4799e5e53ec2babe493c6f259c36a70c543bf1f62778d8c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Entrepartner::Application.config.session_store :cookie_store, key: '_entrepartner_session'
