e9f230419a37696cf364ce895d1007d2855431a1
Matthew Spiers <mspiers@ru1e.com>
2013-11-18 22:09:42 -0500

    Installed Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Entrepartner::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+config.action_mailer.default_url_options = { :host => 'http://morning-cove-3249.herokuapp.com/' }
+
 end
